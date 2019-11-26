c274875c7c41299c9014ee7b392010974f8af808
Ryan Amos <amos2580@hotmail.co.uk>
2019-11-26 16:06:08 +0000

    fix ln 26

--- main.py
+++ main.py
@@ -8,29 +8,24 @@ from flask import Flask
 reg_image = r"(https://)(i\.redd\.it|imgur\.com)(/[a-z0-9]{1,})(\.jpg|\.png)"
 
 if not os.path.exists("config.json"):
-    print("FILE DOES NOT EXIST")
     raw_data = {
-        "prawreddit": {
-            "client_id": "None",
-            "client_secret": "None",
-            "user_agent": "None"
-        }
+        "client_id": "",
+        "client_secret": "",
+        "user_agent": ""
     }
     with open("config.json", "w") as raw_prawcfg:
         json.dump(raw_data, raw_prawcfg)
 
 if os.path.exists("config.json"):
-    print("FILE EXISTS")
     with open("config.json", "r") as raw_prawcfg:
         prawcfg = json.load(raw_prawcfg)
-        prawcfg = prawcfg['prawreddit']
 
 # Can't think of a way to check if values are "None"
-#if ("%s" % prawcfg['client_id']) or prawcfg['client_secret'] or prawcfg['user_agent'] == None:
-#if ("%s" % prawcfg['client_id']) or ("%s" % prawcfg['client_secret']) or ("%s" % prawcfg['user_agent']) == "None":
-#    print(("%s" % prawcfg['client_id']), ("%s" % prawcfg['client_secret']), ("%s" % prawcfg['user_agent']))
-#    print(" [ERROR] Modify the config.")
-#    sys.exit(0)
+print(" [DEBUG] cfg len:", len(prawcfg['client_id']), len(prawcfg['client_secret']), len(prawcfg['user_agent']))
+
+if len(prawcfg['client_id']) or len(prawcfg['client_secret']) or len(prawcfg['user_agent']) == 0:
+    print(" [ERROR] Modify the config.")
+    sys.exit(0)
 
 reddit = praw.Reddit(client_id = prawcfg['client_id'],
                      client_secret = prawcfg['client_secret'],
